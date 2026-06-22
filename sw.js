f69ea5fe881faed6dc636fbf262f083e97f7a922
fifenall-hash <fifenall@gmail.com>
2026-06-22 22:40:59 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,20 @@
-const CACHE_NAME = 'stock-quant-app-v1';
+const CACHE_NAME = 'stock-quant-app-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon.ico'
+  './icon.ico',
+  './icon.png'
 ];
 
-// 설치 단계: 파일들을 캐시(저장)합니다.
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 패치 단계: 인터넷이 끊겨도 캐시된 화면을 보여줍니다.
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // 캐시에 있으면 그것을 반환하고, 없으면 네트워크에서 가져옵니다.
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
